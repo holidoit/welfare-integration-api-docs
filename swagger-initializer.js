8d8bfd97de3b90d93b812e85eadb55928caf198f
Luca Leoni <luca.leoni@holidoit.com>
2026-01-08 09:50:11 +0100

    fix

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -1,24 +1,25 @@
-window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
+window.onload = function () {
+    //<editor-fold desc="Changeable Configuration Block">
 
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUIBundle({
-    urls: [
-      { url: "./openapi.yaml", name: "🇬🇧 English" },
-      { url: "./openapi-it.yaml", name: "🇮🇹 Italiano" }
-    ],
-    "urls.primaryName": "🇬🇧 English",
-    dom_id: '#swagger-ui',
-    deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
-  });
+    // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+    window.ui = SwaggerUIBundle({
+        name: "🌐Seleziona lingua",
+        urls: [
+            {url: "./openapi.yaml", name: "🇬🇧English"},
+            {url: "./openapi-it.yaml", name: "🇮🇹Italiano"}
+        ],
+        "urls.primaryName": "🇬🇧 English",
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+        ],
+        plugins: [
+            SwaggerUIBundle.plugins.DownloadUrl
+        ],
+        layout: "StandaloneLayout"
+    });
 
-  //</editor-fold>
+    //</editor-fold>
 };
